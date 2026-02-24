dafbe96ea075989d352d00ef6f0b7f6b5a0ad909
Arved (Work Laptop) <klojarve@audible.de>
2026-02-24 08:22:43 +0100

    Bump version to 0.1.9

--- smartllm/__init__.py
+++ smartllm/__init__.py
@@ -29,7 +29,7 @@ from . import defaults
 # Advanced users can import: from smartllm.bedrock import BedrockLLMClient
 # Advanced users can import: from smartllm.openai import OpenAILLMClient
 
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 __author__ = "Arved Klöhn"
 
 __all__ = [
